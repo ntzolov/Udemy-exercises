3decc507cadd9b1b887910193170fe67c6511d81
ntzolov <ntzolov@gmail.com>
2023-12-17 16:07:54 +0200

    Small change

--- Next JS/my-app/app/about/page.js
+++ Next JS/my-app/app/about/page.js
@@ -1,3 +1,3 @@
 export default function About() {
-  return <h1 style={{color:'red'}}>This is an about page</h1>;
+  return <h1 style={{ color: 'lightgreen', textAlign: 'center' }}>This is my first Next JS page!</h1>;
 }
